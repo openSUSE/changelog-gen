f09bc3e522adf515da9d968fac9460c986e0b84f
Christian Goll <cgoll@suse.com>
2026-05-12 14:57:50 +0200

    do not env

--- changelog_ai/cli.py
+++ changelog_ai/cli.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Command-line interface for AI changelog generator."""
 
 import argparse
--- changelog_ai/osc_wrapper.py
+++ changelog_ai/osc_wrapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """OSC wrapper for AI changelog generation."""
 
 import argparse
